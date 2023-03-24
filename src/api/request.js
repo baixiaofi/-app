bec75ebdc2312869601df6abc9dbcaae20ec9c11
2799575019 <2799575019@qq.com>
2023-03-24 13:01:30 +0800

    npr

--- src/api/request.js
+++ src/api/request.js
@@ -1,5 +1,6 @@
 import axios  from 'axios'
-
+import nprogress from 'nprogress';
+console.log(nprogress)
 const requests = axios.create({
     // 配置对象
     // 基础路径，发请求的时候，路径当中会出现api
@@ -13,6 +14,8 @@ requests.interceptors.request.use((config) => {
     
     return config
 })
+
+
 // 相应拦截器
 requests.interceptors.response.use((res) => {
     // 成功的回调函数：服务器相应数据回来以后，响应拦截器可以检测到，可以做一些事情
