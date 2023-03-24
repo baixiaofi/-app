034cb89a1870f00afd077243fa0863dad5316f1e
2799575019 <2799575019@qq.com>
2023-03-24 19:53:26 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
 // 引入路由
+import store from '@/store';
 
 import TypeNav from '@/pages/Home/TypeNav';
 Vue.component(TypeNav.name,TypeNav)
@@ -12,7 +13,10 @@ reqgetCategoryList();
 new Vue({
   render: h => h(App),
   // 注册路由
-  router
+  router,
+  // 注册仓库组件身上会多$store属性
+  store,
+
 }).$mount('#app')
 
 
